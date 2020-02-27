961830f2be058ace6ac0161d86c355a1d036ea46
josephineschlegel <josephine.jschlegel@gmail.com>
2020-02-27 10:31:35 +0100

    jose changes

--- script.js
+++ script.js
@@ -55,6 +55,25 @@ function loadVideo(videoinfo){
   document.getElementById(`${videoperson}Player`).src="https://www.youtube.com/embed/"+videoid
 }
 
+function loadVideoDaniel(videoid) {
+    document.getElementById("danielPlayer").src = "https://www.youtube.com/embed/" + videoid
+}
+
+function loadVideoMatt(videoid) {
+    document.getElementById("mattPlayer").src = "https://www.youtube.com/embed/" + videoid
+}
+
+function loadVideoNikola(videoid) {
+    document.getElementById("nikolaPlayer").src = "https://www.youtube.com/embed/" + videoid
+}
+
+function loadVideoLana(videoid) {
+    document.getElementById("lanaPlayer").src = "https://www.youtube.com/embed/" + videoid
+}
+
+function loadVideoJose(videoid) {
+    document.getElementById("josePlayer").src = "https://www.youtube.com/embed/" + videoid
+}
 
 var target = document.getElementById('target');
 var titles = [
